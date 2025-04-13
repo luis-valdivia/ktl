04dc45ba96eabff0df2bd07ccf0741e97a0da084
Luis Valdivia <luisvr0401@gmail.com>
2025-04-13 14:47:37 -0400

    Updates

--- postcss.config.cjs
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-  plugins: [
-    require('@tailwindcss/postcss7-compat'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
-};
--- src/pages/HomePage.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-import HeroComponent from '@/components/HeroComponent.vue'
-</script>
-
-<template>
-  <HeroComponent />
-</template>
